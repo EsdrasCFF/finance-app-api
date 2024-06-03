88f00f08800c9caa76b4140d0e14b8c52c7e6e9b
Esdras Castro <ecff.dev@gmail.com>
2024-06-02 23:39:23 -0300

    chore: typescript setup

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.get("/", async (req, res) => {
+  return res.code(200).send({
+    message: "Success",
+  });
+});
+
 app.listen({ port: 3333 }, () => console.log("Server is running"));
