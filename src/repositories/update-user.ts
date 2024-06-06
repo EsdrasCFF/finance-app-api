c957775effd9d77113a8fc13ed03f9a24348fc3c
Esdras Castro <ecff.dev@gmail.com>
2024-06-06 00:16:48 -0300

    fix: remove validations in repository

--- src/repositories/update-user.ts
+++ src/repositories/update-user.ts
@@ -1,10 +1,10 @@
 import { db } from "../lib/prisma";
 
 interface UpdateUserProps {
-  new_first_name: string | null;
-  new_last_name: string | null;
-  new_email: string | null;
-  new_password: string | null;
+  first_name: string
+  last_name: string
+  email: string
+  password: string
 }
 
 export class UpdateUserRepository {
@@ -17,21 +17,9 @@ export class UpdateUserRepository {
 
     if (!user) return null;
 
-    const { new_first_name, new_last_name, new_email, new_password } =
-      updateUserParams;
-
-    const firstName = new_first_name ? new_first_name : user.first_name;
-    const lastName = new_last_name ? new_last_name : user.last_name;
-    const email = new_email ? new_email : user.email;
-    const password = new_password ? new_password : user.password;
     
     const updatedUser = await db.user.update({
-      data: {
-        first_name: firstName,
-        last_name: lastName,
-        email: email,
-        password: password
-      },
+      data: updateUserParams,
       where: {
         id: userId
       }
