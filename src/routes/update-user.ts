2e845fe68a907388d644f64f2b959f937a8042a0
Esdras Castro <ecff.dev@gmail.com>
2024-06-07 17:03:07 -0300

    feat: add api route to update user

---
+++ src/routes/update-user.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { UpdateUserController } from "../controllers/update-user";
+
+const updatedUserSchema = z.object({
+  first_name: z.string().nullable().optional(),
+  last_name: z.string().nullable().optional(),
+  email: z.string().email().nullable().optional(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be greater or equal 6 characters" })
+    .optional(),
+  old_password: z.string().nullable().optional(),
+});
+
+export async function updateUser(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().patch(
+    "/api/users/:userId",
+    {
+      schema: {
+        params: z.object({
+          userId: z.string().uuid({ message: "Userid provided is not valid" }),
+        }),
+        body: updatedUserSchema,
+        response: {
+          200: z.object({
+            data: z.object({
+              id: z.string(),
+              first_name: z.string(),
+              last_name: z.string(),
+              email: z.string(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { first_name, last_name, email, password, old_password } =
+        request.body;
+      const { userId } = request.params;
+
+      const updateUserController = new UpdateUserController();
+      const user = await updateUserController.execute(userId, {
+        first_name: first_name || null,
+        last_name: last_name || null,
+        email: email || null,
+        password: password || null,
+        old_password: old_password || null,
+      });
+
+      return reply.code(200).send({ data: user });
+    }
+  );
+}
