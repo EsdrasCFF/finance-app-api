44d34f66511276e285820d3ed2078568ffdfb973
Esdras Castro <ecff.dev@gmail.com>
2024-06-10 17:32:58 -0300

    feat: add DeleteUser cpntroller

---
+++ src/controllers/delete-user.ts
@@ -0,0 +1,25 @@
+import { User } from "@prisma/client";
+import validator from "validator";
+import { BadRequest } from "../routes/_errors/bad-request";
+import { IDeleteUserService } from "../services/delete-user";
+
+export interface IDeleteUserController {
+  execute(userId: string): Promise<User>
+}
+
+export class DeleteUserController implements IDeleteUserController {
+
+  constructor(private deleteUserService: IDeleteUserService) {}
+
+  async execute(userId: string) {
+    const idIsValid = validator.isUUID(userId)
+
+    if(!idIsValid) {
+      throw new BadRequest('Provided UserId is not valid!')
+    }
+
+    const deletedUser = await this.deleteUserService.execute(userId)
+
+    return deletedUser
+  }
+}
