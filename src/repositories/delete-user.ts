fe502b8d18e4c5465ffa8f73cb0e4257119d6266
Esdras Castro <ecff.dev@gmail.com>
2024-06-10 17:32:26 -0300

    feat: add DeleteUser repository

---
+++ src/repositories/delete-user.ts
@@ -0,0 +1,18 @@
+import { User } from "@prisma/client";
+import { db } from "../lib/prisma";
+
+export interface IDeleteUserRepository {
+  execute(userId: string): Promise<User | null>
+}
+
+export class DeleteUserRepository implements IDeleteUserRepository {
+  async execute(userId: string) {
+    const user = await db.user.delete({
+      where: {
+        id: userId
+      }
+    })
+
+    return user || null
+  }
+}
