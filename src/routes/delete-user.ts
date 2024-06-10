5bff0bab59ce8c1b7dcf61c7e9e1ee311c31b9e4
Esdras Castro <ecff.dev@gmail.com>
2024-06-10 17:33:22 -0300

    feat: add api route to delete user by id

---
+++ src/routes/delete-user.ts
@@ -0,0 +1,44 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { DeleteUserRepository } from "../repositories/delete-user";
+import { DeleteUserService } from "../services/delete-user";
+import { DeleteUserController } from "../controllers/delete-user";
+
+export async function deleteUser(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().delete(
+    "/api/users/:userId",
+    {
+      schema: {
+        params: z.object({
+          userId: z.string().uuid({message: 'Provided userId is not valid'})
+        }),
+        response: {
+          200: z.object({
+            data: z.object({
+              id: z.string(),
+              first_name: z.string(),
+              last_name: z.string(),
+              email: z.string()
+            })
+          })
+        }
+      }
+    },
+    async(request, reply) => {
+      const { userId } = request.params
+      
+      const deleteUserRepository = new DeleteUserRepository()
+
+      const deleteUserService = new DeleteUserService(deleteUserRepository)
+      
+      const deleteUserController = new DeleteUserController(deleteUserService)
+
+      const deletedUser = await deleteUserController.execute(userId)
+
+      const { password, ...otherProps } = deletedUser;
+
+      return reply.code(200).send({data: otherProps})
+    }
+  )
+}
