3bf343d2230a396de62f047284d2a96609b0d866
Esdras Castro <ecff.dev@gmail.com>
2024-06-11 17:50:46 -0300

    refactor: move users routes to its own folder

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
-import { createUser } from "./routes/create-user";
+import { createUser } from "./routes/users/create-user";
 import { errorHandler } from "./error-handler";
-import { getUserById } from "./routes/get-user-by-id";
-import { updateUser } from "./routes/update-user";
-import { deleteUser } from "./routes/delete-user";
+import { getUserById } from "./routes/users/get-user-by-id";
+import { updateUser } from "./routes/users/update-user";
+import { deleteUser } from "./routes/users/delete-user";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
