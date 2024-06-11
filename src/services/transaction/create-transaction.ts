6cc487a50362b42e1a930c74a4485197736e9228
Esdras Castro <ecff.dev@gmail.com>
2024-06-11 14:35:11 -0300

    feat: add CreateTransactionService

---
+++ src/services/transaction/create-transaction.ts
@@ -0,0 +1,29 @@
+import { Transaction } from "@prisma/client";
+import { ICreateTransactionRepository } from "../../repositories/transaction/create-transaction";
+import { IGetUserByIdRepository } from "../../repositories/user/get-user-by-id";
+import { NotFound } from "../../routes/_errors/not-found";
+
+export interface ICreateTransactionService {
+  execute(createTransactionParams: Omit<Transaction, 'id'>): Promise<Transaction>
+}
+
+export class CreateTransactionService implements ICreateTransactionRepository {
+  constructor(
+    private createTransactionRepository: ICreateTransactionRepository,
+    private getUserByIdRepository: IGetUserByIdRepository
+  ) {}
+
+  async execute(createTransactionParams: Omit<Transaction, 'id'>) {
+    const userId = createTransactionParams.user_id
+
+    const user = await this.getUserByIdRepository.execute(userId)
+
+    if(!user) {
+      throw new NotFound('User not found!')
+    }
+
+    const transaction = await this.createTransactionRepository.execute(createTransactionParams)
+  
+    return transaction
+  }
+}
