416767d8139cf1b5045faf4cc9c697429cf47c02
Esdras Castro <ecff.dev@gmail.com>
2024-06-22 18:34:22 -0300

    feat: add GetTransactionByIdRepository

---
+++ src/repositories/transaction/get-transaction-by-id.ts
@@ -0,0 +1,18 @@
+import { Transaction } from "@prisma/client";
+import { db } from "../../lib/prisma";
+
+export interface IGetTransactionByIdRepository {
+  execute(transactionId: string): Promise<Transaction | null>
+}
+
+export class GetTransactionByIdRepository implements IGetTransactionByIdRepository {
+  async execute(transactionId: string) {
+    const transaction = await db.transaction.findUnique({
+      where: {
+        id: transactionId
+      }
+    })
+
+    return transaction
+  }
+}
