da64a77cc7ef9a130095f447b527e725a555f1cd
Esdras Castro <ecff.dev@gmail.com>
2024-06-23 18:00:12 -0300

    fix: ensure correct type to return of the query.

--- src/services/user/get-user-balance.ts
+++ src/services/user/get-user-balance.ts
@@ -1,6 +1,15 @@
-import { BalanceParams, IGetUserBalanceRepository } from "../../repositories/user/get-user-balance";
+import { convertHundredUnitsToAmount } from "../../lib/utils";
+import { IGetUserBalanceRepository } from "../../repositories/user/get-user-balance";
 import { IGetUserByIdRepository } from "../../repositories/user/get-user-by-id";
 import { NotFound } from "../../routes/_errors/not-found";
+
+export interface BalanceParams {
+  incomes: number 
+  expenses: number 
+  investments: number 
+  balance: number 
+}
+
 
 export interface IGetUserBalanceService {
   execute(userId: string): Promise<BalanceParams>
@@ -19,7 +28,14 @@ export class GetUserBalanceService implements IGetUserBalanceService {
       throw new NotFound('User not found!')
     }
 
-    const balance = await this.getUserBalanceRepository.execute(userId)
+    const result = await this.getUserBalanceRepository.execute(userId)
+
+    const balance = {
+      incomes: convertHundredUnitsToAmount(result.incomes),
+      expenses: convertHundredUnitsToAmount(result.expenses),
+      investments: convertHundredUnitsToAmount(result.investments),
+      balance: convertHundredUnitsToAmount(result.balance),
+    }
 
     return balance
   }
