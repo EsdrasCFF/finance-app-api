3bf64bcf8fdfcbd58a1584240a1c5f2b44f33c9d
Esdras Castro <ecff.dev@gmail.com>
2024-06-23 17:05:05 -0300

    feat: add GetUserBalanceRepository

---
+++ src/repositories/user/get-user-balance.ts
@@ -0,0 +1,35 @@
+import { db } from "../../lib/prisma"
+
+interface BalanceParams {
+  incomes: number,
+  expenses: number,
+  investments: number,
+  balance: number
+}
+
+interface IGetUserBalanceRepository {
+  execute(userId: string): Promise<BalanceParams>
+}
+
+export class GetUserBalanceRepository implements IGetUserBalanceRepository{
+  async execute(userId: string) {
+    const balance = await db.$queryRaw<BalanceParams>`
+      SELECT 
+        SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
+        SUM(CASE WHEN type = 'INCOME' THEN amount ELSE 0 END) AS incomes,
+        SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
+        (
+          SUM(CASE WHEN type = 'INCOME' THEN amount ELSE 0 END)
+          - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
+          - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
+        ) AS balance
+      
+      FROM 
+        transactions
+      WHERE
+          user_id = ${userId}
+    `
+  
+    return balance
+  }
+}
