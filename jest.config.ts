e99aac200f7bcd90ced4e586caaeff203ba915b6
Esdras Castro <ecff.dev@gmail.com>
2024-06-24 13:16:34 -0300

    feat: add jest.config file

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts']
+};
+
+export default config;
