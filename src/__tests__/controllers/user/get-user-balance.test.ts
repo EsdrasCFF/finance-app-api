cf989ec9c2aec0a599d92596cec8412fe8edfd95
Esdras Castro <ecff.dev@gmail.com>
2024-06-25 14:04:46 -0300

    test: ensure that GetUserBalance throw error if userId is invalid and returns balanceParams

---
+++ src/__tests__/controllers/user/get-user-balance.test.ts
@@ -0,0 +1,62 @@
+import { faker } from "@faker-js/faker"
+import { BalanceParams, IGetUserBalanceService } from "../../../services/user/get-user-balance"
+import validator from "validator"
+import { BadRequest } from "../../../routes/_errors/bad-request"
+import { GetUserBalanceController } from "../../../controllers/user/get-user-balance"
+
+describe('GetUserBalanceController', () => {
+
+  const balanceParams = {
+    incomes: faker.number.int({min: 1500, max: 2000}),
+    expenses: faker.number.int({min: 800, max: 1600}),
+    investments: faker.number.int({min: 100, max: 600}),
+    balance: faker.number.int({min: 10, max: 500})
+  }
+
+  const userIdParams = faker.string.uuid()
+
+  class GetUserBalanceServiceStub implements IGetUserBalanceService {
+    async execute(userId: string) {
+      const userIdIsValid = validator.isUUID(userId)
+
+      if(!userIdIsValid) {
+        throw new BadRequest('Provided UserId is not valid!')
+      }
+
+      return balanceParams
+    }
+  }
+
+  const makeSut = () => {
+    const getUserBalanceServiceStub = new GetUserBalanceServiceStub()
+    const sut = new GetUserBalanceController(getUserBalanceServiceStub)
+  
+    return { getUserBalanceServiceStub, sut }
+  }
+
+  it('Should return user balance data with valid userId', async () => {
+    //arrange
+    const { sut } = makeSut()
+    
+
+    //act
+    const result = await sut.execute(userIdParams)
+
+    //assert
+    expect(result).toEqual(balanceParams)
+    expect(result).not.toBeFalsy()
+    expect(result).not.toBeUndefined()
+    
+  })
+
+  it('Should throw BadRequest error if userId provided is not valid!', async () => {
+    //arrange
+    const { sut } = makeSut()
+  
+    //act
+    const result = sut.execute(faker.string.alpha({length: {min: 5, max: 15}}))
+
+    //assert
+    await expect(result).rejects.toThrow(BadRequest)
+  })
+})
