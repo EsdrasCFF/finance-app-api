d079842462d05a56cd445f0f61452c065d21c691
Esdras Castro <ecff.dev@gmail.com>
2024-06-25 14:49:06 -0300

    test: ensure that GetUserById returns user data if provided userId is valid and is found

---
+++ src/__tests__/controllers/user/get-user-by-id.test.ts
@@ -0,0 +1,51 @@
+import { faker } from "@faker-js/faker"
+import { IGetUserByIdService } from "../../../services/user/get-user-by-id"
+import validator from "validator"
+import { BadRequest } from "../../../routes/_errors/bad-request"
+import { GetUserByIdController } from "../../../controllers/user/get-user-by-id"
+
+describe('GetUserByIdController', () => {
+  
+  const userIdParams = faker.string.uuid()
+
+  const userData = {
+    id: faker.string.uuid(),
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({length: 7})
+  }
+
+  class GetUserByIdServiceStub implements IGetUserByIdService {
+    async execute(userId: string) {
+      const userIdIsValid = validator.isUUID(userId)
+
+      if(!userIdIsValid) {
+        throw new BadRequest()
+      }
+
+      return userData
+    }
+  }
+
+  const makeSut = () => {
+    const getUserByIdServiceStub = new GetUserByIdServiceStub()
+    const sut = new GetUserByIdController(getUserByIdServiceStub)
+
+    return {getUserByIdServiceStub, sut}
+  }
+
+  it('Should return user data if userId provided is valid and if user is found!', async () => {
+    //arrange
+    const { sut } = makeSut()
+
+    //act
+    const result = await sut.execute(userIdParams)
+
+    //assert
+    expect(result.email).toEqual(userData.email)
+    expect(result.first_name).toEqual(userData.first_name)
+    expect(result.id).toEqual(userData.id)
+    expect(result.email).toEqual(userData.email)
+  })
+})
