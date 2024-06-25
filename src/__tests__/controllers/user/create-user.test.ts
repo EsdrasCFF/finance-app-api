aa26bf4a7b1ad2461bab34262e491d0f9ebc19dc
Esdras Castro <ecff.dev@gmail.com>
2024-06-24 21:16:08 -0300

    test: add more test on the create-user

--- src/__tests__/controllers/user/create-user.test.ts
+++ src/__tests__/controllers/user/create-user.test.ts
@@ -3,7 +3,7 @@ import { CreateUserController } from "../../../controllers/user/create-user"
 import { ICreateUserService } from "../../../services/user/create-user"
 import { v4 as uuidv4 } from 'uuid'
 
-describe('Create User controller', () => {
+describe('Create user controller', () => {
 
   class CreateUserServiceStub implements ICreateUserService {
     async execute(user: Omit<User, 'id'>) {
@@ -30,7 +30,7 @@ describe('Create User controller', () => {
     const createUserParams = {
       first_name: 'Esdras',
       last_name: 'Castro',
-      email: 'esdras@email.com',
+      email: 'esdras@emai.com',
       password: '123456'
     }
     
@@ -42,5 +42,48 @@ describe('Create User controller', () => {
     expect(result.first_name).toBeTruthy()
     expect(result.email).not.toBeNull()
     expect(result).not.toBeUndefined()
+  })
+
+  it('Should return null if provided first_name is falsy', async () => {
+    //arrange
+    const crateUserServiceStub = new CreateUserServiceStub()
+    const createUserController = new CreateUserController(crateUserServiceStub)
+
+    const createUserParams = {
+      first_name: '',
+      last_name: 'Castro',
+      email: '',
+      password: '123456'
+    }
+
+    //act
+
+    const result = await createUserController.execute(createUserParams)
+
+    //assert
+
+    expect(result.first_name).toBeFalsy()
+    expect(result.email).toBeFalsy()
+  })
+
+  it('Should error if email or last_name are not provided', async () => {
+    //arrange
+    const crateUserServiceStub = new CreateUserServiceStub()
+    const createUserController = new CreateUserController(crateUserServiceStub)
+
+    const createUserParams = {
+      first_name: 'Esdras',
+      last_name: '',
+      email: '',
+      password: '123456'
+    }
+
+    //act
+    const result = await createUserController.execute(createUserParams)
+
+
+    //assert
+    expect(result.last_name).toBeFalsy()
+    expect(result.email).toBeFalsy()
   })
 })
