3a809e17beb7bc18d7c711ffe9f299228f923826
Esdras Castro <ecff.dev@gmail.com>
2024-06-26 09:49:58 -0300

    test: ensure that transaction is created successfully

---
+++ src/__tests__/controllers/transaction/create-transaction.test.ts
@@ -0,0 +1,52 @@
+import { faker } from "@faker-js/faker";
+import { ICreateTransactionService } from "../../../services/transaction/create-transaction";
+import { $Enums, TRANSACTION_TYPE, Transaction } from "@prisma/client";
+import { CreateTransactionController } from "../../../controllers/transaction/create-transaction";
+
+describe('CreateTransactionController', () => {
+
+  const createTransactionParams = {
+    name: faker.person.firstName(),
+    description: faker.definitions.lorem.words[0],
+    date: faker.date.recent(),
+    amount: faker.finance.amount(),
+    type: 'INCOME' as TRANSACTION_TYPE,
+    userId: faker.string.uuid()
+  }
+
+  const userIdParams = faker.string.uuid()
+
+  class CreateTransactionServiceStub implements ICreateTransactionService {
+    async execute(createTransactionParams: Omit<Transaction, 'id'>) {
+      
+      const transactionData = { 
+        ...createTransactionParams,
+        id: faker.string.uuid()
+      }
+
+      return transactionData
+    }
+
+  }
+
+  const makeSut = () => {
+    const createTransactionServiceStub = new CreateTransactionServiceStub()
+    const sut = new CreateTransactionController(createTransactionServiceStub)
+  
+    return {sut,  createTransactionServiceStub}
+  }
+
+  it('Should return transaction data if create transaction ocurred successfully', async () => {
+    //arrange
+    const {sut} = makeSut()
+    
+    //act
+    const result = await sut.execute(createTransactionParams)
+    
+    //assert
+    expect(result.amount).not.toBeNaN()
+    expect(result.type).toEqual(createTransactionParams.type)
+    expect(result.name).toEqual(createTransactionParams.name)
+    expect(result.id).toBeTruthy()
+  })
+})
