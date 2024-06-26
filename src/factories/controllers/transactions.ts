f19bcc4155c728e694fe9c30e0b608790658bc1f
Esdras Castro <ecff.dev@gmail.com>
2024-06-25 23:35:07 -0300

    feat: add makeDeleteTransaction factory

--- src/factories/controllers/transactions.ts
+++ src/factories/controllers/transactions.ts
@@ -1,12 +1,15 @@
 import { CreateTransactionController } from "../../controllers/transaction/create-transaction";
+import { DeleteTransactionController } from "../../controllers/transaction/delete-transaction";
 import { GetTransactionsByUserIdController } from "../../controllers/transaction/get-transactions-by-userId";
 import { UpdateTransactionController } from "../../controllers/transaction/update-transaction";
 import { CreateTransactionRepository } from "../../repositories/transaction/create-transaction";
+import { DeleteTransactionRepository } from "../../repositories/transaction/delete-transaction";
 import { GetTransactionByIdRepository } from "../../repositories/transaction/get-transaction-by-id";
 import { GetTransactionsByUserIdRepository } from "../../repositories/transaction/get-transactions-by-userId";
 import { UpdateTransactionRepository } from "../../repositories/transaction/update-transactions";
 import { GetUserByIdRepository } from "../../repositories/user/get-user-by-id";
 import { CreateTransactionService } from "../../services/transaction/create-transaction";
+import { DeleteTransactionService } from "../../services/transaction/delete-transaction";
 import { GetTransactionsByUserIdService } from "../../services/transaction/get-transactions-by-userId";
 import { UpdateTransactionService } from "../../services/transaction/update-transaction";
 
@@ -41,4 +44,14 @@ export function makeUpdateTransactionController () {
   const updateTransactionController = new UpdateTransactionController(updateTransactionService)
 
   return updateTransactionController
+}
+
+export function makeDeleteTransactionController() {
+  const deleteTransactionRepository = new DeleteTransactionRepository()
+  const getTransactionByIdRepository = new GetTransactionByIdRepository()
+
+  const deleteTransactionService = new DeleteTransactionService(deleteTransactionRepository, getTransactionByIdRepository)
+  const deleteTransactionController = new DeleteTransactionController(deleteTransactionService)
+
+  return deleteTransactionController
 }
