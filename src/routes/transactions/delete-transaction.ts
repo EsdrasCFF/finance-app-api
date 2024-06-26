cf9fe8ee592a78f282aaf1b54ef431fdbe6b9a5f
Esdras Castro <ecff.dev@gmail.com>
2024-06-25 23:37:24 -0300

    feat: add delete-transaction route

---
+++ src/routes/transactions/delete-transaction.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { makeDeleteUserController } from "../../factories/controllers/users";
+import { makeDeleteTransactionController } from "../../factories/controllers/transactions";
+
+export async function deleteUser(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().delete(
+    "/api/transaction/:transactionId",
+    {
+      schema: {
+        params: z.object({
+          transactionId: z.string().uuid({message: 'Provided transactionId is not valid'})
+        }),
+        response: {
+          200: z.object({
+            data: z.object({
+              id: z.string(),
+              user_id: z.string(),
+              description: z.string().nullable(),
+              date: z.coerce.date(),
+              amount: z.number(),
+              type: z.string(),
+            })
+          })
+        }
+      }
+    },
+    async(request, reply) => {
+      const { transactionId } = request.params
+  
+      const deleteTransactionController = makeDeleteTransactionController()
+
+      const deletedUser = await deleteTransactionController.execute(transactionId)
+
+      return reply.code(200).send({data: deletedUser})
+    }
+  )
+}
+
+
+
+
+// id: string;
+//     user_id: string;
+//     name: string;
+//     description: string | null;
+//     date: Date;
+//     amount: number;
+//     type: $Enums.TRANSACTION_TYPE;
