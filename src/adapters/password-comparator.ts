becaa58d51bbc846aea4f35825a3cc0d10dc21eb
Esdras Castro <ecff.dev@gmail.com>
2024-06-27 09:39:43 -0300

    feat: add PasswordComparatorAdapter

---
+++ src/adapters/password-comparator.ts
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt"
+
+export interface IPasswordComparatorAdapter {
+  execute(old_password: string, encrypted_old_password: string): Promise<boolean> 
+}
+
+export class PasswordComparatorAdapter implements IPasswordComparatorAdapter {
+  async execute(old_password: string, encrypted_old_password: string) {
+    const passwordCompare = await bcrypt.compare(old_password, encrypted_old_password)
+
+    return passwordCompare
+  }
+
+}
