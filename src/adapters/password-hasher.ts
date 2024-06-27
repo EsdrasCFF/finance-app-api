8863d540064ad6349233c3a29e04d0a32f7a2038
Esdras Castro <ecff.dev@gmail.com>
2024-06-27 00:30:25 -0300

    feat: add PasswordHasher adapter

---
+++ src/adapters/password-hasher.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt'
+
+export interface IPasswordHasherAdapter {
+  execute(password: string): Promise<string>
+}
+
+export class PasswordHasherAdapter implements IPasswordHasherAdapter{
+  async execute(password: string) {
+    const passwordHashed = await bcrypt.hash(password, 9)
+
+    return passwordHashed
+  }
+}
