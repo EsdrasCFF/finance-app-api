dafa007fafdee5327051262fced7709682d17d43
Esdras Castro <ecff.dev@gmail.com>
2024-06-27 23:50:58 -0300

    test: add tests on DeleteUserService

---
+++ src/__tests__/services/user/delete-user.test.ts
@@ -0,0 +1,83 @@
+import { faker } from "@faker-js/faker"
+import { IDeleteUserRepository } from "../../../repositories/user/delete-user"
+import { DeleteUserService } from "../../../services/user/delete-user"
+import { IGetUserByIdRepository } from "../../../repositories/user/get-user-by-id"
+import { NotFound } from "../../../routes/_errors/not-found"
+
+describe('DeleteUserService', () => {
+
+  const createUserParams = {
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({length: 7})
+  }
+  
+  const userIdParams = faker.string.uuid()
+  
+  class DeleteUserRepositoryStub implements IDeleteUserRepository {
+    async execute(userId: string) {
+      return {
+        ...createUserParams,
+        id: userIdParams
+      }
+    }
+  }
+
+  class GetUserByIdRepositoryStub implements IGetUserByIdRepository {
+    async execute(userId: string) {
+      return {
+        ...createUserParams,
+        id: userId
+      }
+    }
+  }
+
+  const makeSut = () => {
+    const deleteUserRepositoryStub = new DeleteUserRepositoryStub()
+    const getUserByIdRepositoryStub = new GetUserByIdRepositoryStub()
+    const sut = new DeleteUserService(deleteUserRepositoryStub, getUserByIdRepositoryStub)
+  
+    return {sut, deleteUserRepositoryStub, getUserByIdRepositoryStub}
+  }
+
+  it('Should return user data that was deleted', async () => {
+    //arrange
+    const { sut } = makeSut()
+
+    //act
+    const result = await sut.execute(userIdParams)
+
+    //assert
+    expect(result).toEqual({...createUserParams, id: userIdParams})
+
+  })
+
+  it('Should throw BadRequest error if user not found', async () => {
+    //arrange
+    const { sut, getUserByIdRepositoryStub } = makeSut()
+    
+    //@ts-ignore
+    jest.spyOn(getUserByIdRepositoryStub, 'execute').mockReturnValueOnce(null)
+
+    //act
+    const result = sut.execute(userIdParams)
+  
+    //assert
+    await expect(result).rejects.toThrow(NotFound)
+  })
+
+  it('Should call DeleteUserRepository with correct params', async () => {
+    //arrange
+    const { deleteUserRepositoryStub, sut } = makeSut()
+  
+    const executeSpy = jest.spyOn(deleteUserRepositoryStub, 'execute')
+
+    //act
+    await sut.execute(userIdParams)
+
+    //assert
+    expect(executeSpy).toHaveBeenCalledWith(userIdParams)
+  })
+
+})
