42e6675c422224257346ead3105f6542bf17664e
Esdras Castro <ecff.dev@gmail.com>
2024-06-28 00:43:11 -0300

    test: ensure that user data is return

---
+++ src/__tests__/services/user/get-user-by-email.test.ts
@@ -0,0 +1,46 @@
+import { faker } from "@faker-js/faker"
+import { IGetUserByEmailRepository } from "../../../repositories/user/get-user-by-email"
+import { GetUserByEmailService } from "../../../services/user/get-user-by-email"
+
+describe('GetUserByEmailServie', () => {
+  
+  const userIdParams = faker.string.uuid()
+
+  const userData = {
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({length: 7})
+  }
+  
+  class GetUserByEmailRepositoryStub implements IGetUserByEmailRepository {
+    async execute(email: string) {
+      return {
+        ...userData,
+        email: email,
+        id: userIdParams
+      }
+    }
+  }
+
+
+  const makeSut = () => {
+    const getUserByEmailRepositoryStub = new GetUserByEmailRepositoryStub()
+
+    const sut = new GetUserByEmailService(getUserByEmailRepositoryStub)
+  
+    return {sut, getUserByEmailRepositoryStub}
+  }
+
+  it('Should return user data if found user b email', async () => {
+    //arrange
+    const { sut, getUserByEmailRepositoryStub } = makeSut()
+  
+    //act
+    const result = await sut.execute(userData.email)
+  
+    //assert
+    expect(result).toEqual({...userData, id: userIdParams})
+  })
+
+})
