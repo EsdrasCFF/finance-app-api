faa0cc3ac73e075c056465011a7fc56db1d72803
Esdras Castro <ecff.dev@gmail.com>
2024-06-27 21:07:40 -0300

    test: ensure that CreateUserService return created user successfully

---
+++ src/__tests__/services/user/create-user.test.ts
@@ -0,0 +1,79 @@
+import { fa, faker } from "@faker-js/faker"
+import { ICreateUserRepository } from "../../../repositories/user/create-users"
+import { IGetUserByEmailRepository } from "../../../repositories/user/get-user-by-email"
+import { User } from "@prisma/client"
+import { IPasswordComparatorAdapter } from "../../../adapters/password-comparator"
+import { IPasswordHasherAdapter } from "../../../adapters/password-hasher"
+import { CreateUserService } from "../../../services/user/create-user"
+import { BadRequest } from "../../../routes/_errors/bad-request"
+
+describe('CreateUserService', () => {
+  
+  const createUserParams = {
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({length: 7})
+  }
+  
+  class GetUserByEmailRepositoryStub implements IGetUserByEmailRepository {
+    async execute(email: string) {
+      return null
+    }
+  }
+
+  class CreateUserRepositoryStub implements ICreateUserRepository {
+    async execute(createUserParams: Omit<User, 'id'>) {
+      const userData = {
+        ...createUserParams,
+        id: faker.string.uuid()
+      }
+
+      return userData
+    }
+  }
+
+  class PasswordHasherAdapterStub implements IPasswordHasherAdapter {
+    async execute(password: string) {
+      return `${password}hashed_password`
+    }
+  }
+
+
+
+  const makeSut = () => {
+    const createUserRepository = new CreateUserRepositoryStub()
+    const getUserByEmailRepository = new GetUserByEmailRepositoryStub()
+    const passwordHasherAdapter = new PasswordHasherAdapterStub()
+
+    
+    const sut = new CreateUserService(
+      createUserRepository,
+      getUserByEmailRepository,
+      passwordHasherAdapter
+    )
+    
+    return {
+      sut,
+      createUserRepository,
+      getUserByEmailRepository,
+      passwordHasherAdapter,
+    }
+  }
+
+  it('Should return a created user successfully', async () => {
+    //arrange
+    const { sut } = makeSut()
+
+
+    //act
+    const result = await sut.execute(createUserParams)
+
+    //assert
+    expect(result).toBeTruthy()
+    expect(result.first_name).toEqual(createUserParams.first_name)
+    expect(result.password).toBeTruthy()
+  })
+
+  
+})
