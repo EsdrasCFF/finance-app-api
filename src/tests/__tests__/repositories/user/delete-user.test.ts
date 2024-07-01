3499c9ca84682cc714ff7db2d980087f7af47b1d
Esdras Castro <ecff.dev@gmail.com>
2024-07-01 16:46:28 -0300

    test: ensure that DeleteUserRepository calls database with correct params

--- src/tests/__tests__/repositories/user/delete-user.test.ts
+++ src/tests/__tests__/repositories/user/delete-user.test.ts
@@ -1,6 +1,6 @@
 import { db } from "../../../../lib/prisma"
 import { DeleteUserRepository } from "../../../../repositories/user/delete-user"
-import { createUserParams } from "../../../fixtures/user"
+import { createUserParams, userIdParams } from "../../../fixtures/user"
 
 describe('DeleteUserRepository', () => {
   
@@ -16,4 +16,20 @@ describe('DeleteUserRepository', () => {
     expect(createdUser).toStrictEqual(result)
   })
 
+  it('Should call DataBase with correct params', async () => {
+    const sut = new DeleteUserRepository()
+    const dbSpy = jest.spyOn(db.user, 'delete')
+
+    const createdUser = await db.user.create({
+      data: createUserParams
+    })
+
+    await sut.execute(createdUser.id)
+
+    expect(dbSpy).toHaveBeenCalledWith({
+      where: {
+        id: createdUser.id
+      }
+    })
+  })
 })
