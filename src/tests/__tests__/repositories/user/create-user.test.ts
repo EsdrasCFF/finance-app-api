0537747ee2754c46f2f5a2caee8feb141d6c9a76
Esdras Castro <ecff.dev@gmail.com>
2024-07-01 16:36:29 -0300

    test: ensure that db is called with correct params

--- src/tests/__tests__/repositories/user/create-user.test.ts
+++ src/tests/__tests__/repositories/user/create-user.test.ts
@@ -1,3 +1,4 @@
+import { db } from "../../../../lib/prisma"
 import { CreateUserRepository } from "../../../../repositories/user/create-users"
 import { createUserParams } from "../../../fixtures/user"
 
@@ -10,5 +11,16 @@ describe('CreateUserRepository', () => {
     expect(result).toBeTruthy()
     expect(result.email).toEqual(createUserParams.email)
     expect(result.first_name).toEqual(createUserParams.first_name)
+  })
+
+  it('Should call DB with correct params', async () => {
+    const sut = new CreateUserRepository()
+    const dbSpy = jest.spyOn(db.user, 'create')
+
+    await sut.execute(createUserParams)
+
+    expect(dbSpy).toHaveBeenCalledWith({
+      data: createUserParams
+    })
   })
 })
