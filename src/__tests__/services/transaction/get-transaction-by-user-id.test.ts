151ca8590a82c0c7a0318bb852562c966e6ccc7c
Esdras Castro <ecff.dev@gmail.com>
2024-06-30 21:33:01 -0300

    test: ensure that Transactions Data is returnd successfully

---
+++ src/__tests__/services/transaction/get-transaction-by-user-id.test.ts
@@ -0,0 +1,69 @@
+import { faker } from "@faker-js/faker"
+import { IGetTransactionsByUserIdRepository } from "../../../repositories/transaction/get-transactions-by-userId"
+import { GetTransactionsByUserIdService } from "../../../services/transaction/get-transactions-by-userId"
+import { TRANSACTION_TYPE } from "@prisma/client"
+import { IGetUserByIdRepository } from "../../../repositories/user/get-user-by-id"
+
+describe('GetTransactionByUserIdRepository', () => {
+
+  const userIdParams = faker.string.uuid()
+  
+  const transactionData = {
+    id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    amount: faker.number.float({min: 100.50, max: 500.50}),
+    date: faker.date.recent(),
+    user_id: userIdParams,
+    type: 'INCOME' as TRANSACTION_TYPE
+  }
+
+  const transactions = [transactionData]
+
+  const userData = {
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({length: 8})
+  }
+
+  class GetTransactionsByUserIdRepositoryStub implements IGetTransactionsByUserIdRepository {
+    async execute(userId: string) {
+
+      return transactions
+    }
+  }
+
+  class GetUserByIdRepositoryStub implements IGetUserByIdRepository {
+    async execute(userId: string) {
+      return {
+        ...userData,
+        id: userId
+      }
+    }
+  }
+
+  const makeSut = () => {
+    const getTransactionsByIdRepositoryStub = new GetTransactionsByUserIdRepositoryStub()
+    const getUserByIdRepositoryStub = new GetUserByIdRepositoryStub()
+
+    const sut = new GetTransactionsByUserIdService(
+      getTransactionsByIdRepositoryStub,
+      getUserByIdRepositoryStub
+    )
+
+    return {sut, getTransactionsByIdRepositoryStub, getUserByIdRepositoryStub}
+  }
+
+  it('Should return transaction data successfully', async () => {
+    // arrange
+    const { sut } = makeSut()
+
+    // act
+    const result = await sut.execute(userIdParams)
+
+    // assert
+    expect(result).toBeTruthy()
+    expect(result[0].user_id).toEqual(userIdParams)
+  })
+})
