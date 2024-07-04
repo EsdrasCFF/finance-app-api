3c5ba57ecff1d67c76ef875f0e2d0d2a57ed9fef
Esdras Castro <ecff.dev@gmail.com>
2024-07-04 20:21:21 -0300

    test: ensure that Prisma is updated with correct params

--- src/tests/__tests__/repositories/user/update-user.test.ts
+++ src/tests/__tests__/repositories/user/update-user.test.ts
@@ -23,4 +23,33 @@ describe('UpdateUserRepository', () => {
     expect(result.first_name).toEqual(first_name)
     expect(result.last_name).toEqual(last_name)
   })
+
+  it('Should call Prisma with correct params', async () => {
+    const user = await db.user.create({
+      data: createUserParams
+    })
+
+    const first_name = faker.person.firstName()
+    const last_name = faker.person.lastName()
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    
+    const sut = new UpdateUserRepository()
+
+    const dbSpy = jest.spyOn(db.user, 'update')
+
+    await sut.execute(user.id, {first_name, last_name, email, password})
+
+    expect(dbSpy).toHaveBeenCalledWith({
+      data: {
+        first_name,
+        last_name,
+        email,
+        password
+      },
+      where: {
+        id: user.id
+      }
+    })
+  })
 })
