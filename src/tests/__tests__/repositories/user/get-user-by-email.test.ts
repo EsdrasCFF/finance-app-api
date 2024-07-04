c033e845da78926d4fe2c711c2194b533a8e9a78
Esdras Castro <ecff.dev@gmail.com>
2024-07-04 20:03:44 -0300

    test: ensure GetUserByEmailRepositor is called with correct params

--- src/tests/__tests__/repositories/user/get-user-by-email.test.ts
+++ src/tests/__tests__/repositories/user/get-user-by-email.test.ts
@@ -1,3 +1,4 @@
+import { sign } from "crypto"
 import { db } from "../../../../lib/prisma"
 import { GetUserByEmailRepository } from "../../../../repositories/user/get-user-by-email"
 import { createUserParams } from "../../../fixtures/user"
@@ -11,5 +12,21 @@ describe('GetUserByEmailRepository', () => {
     const result = await sut.execute(user.email)
 
     expect(result?.email).toStrictEqual(user.email)
+  })
+
+  it('Should call Prisma with correct params', async () => {
+    const user = await db.user.create({data: createUserParams})
+    
+    const sut = new GetUserByEmailRepository()
+
+    const dbSpy = jest.spyOn(db.user, 'findFirst')
+
+    await sut.execute(user.email)
+
+    expect(dbSpy).toHaveBeenCalledWith({
+      where: {
+        email: user.email
+      }
+    })
   })
 })
