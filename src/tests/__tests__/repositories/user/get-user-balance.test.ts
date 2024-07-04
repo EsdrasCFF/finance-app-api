c5bf561628517b7be05ed035aaf7faab888ebec1
Esdras Castro <ecff.dev@gmail.com>
2024-07-04 19:53:11 -0300

    test: ensure that queryRaw is working correctly

--- src/tests/__tests__/repositories/user/get-user-balance.test.ts
+++ src/tests/__tests__/repositories/user/get-user-balance.test.ts
@@ -1,3 +1,4 @@
+import { GetUserBalanceController } from "../../../../controllers/user/get-user-balance"
 import { db } from "../../../../lib/prisma"
 import { GetUserBalanceRepository } from "../../../../repositories/user/get-user-balance"
 import { createUserParams } from "../../../fixtures/user"
@@ -61,5 +62,65 @@ describe('GetUserBalanceRepository', () => {
     expect(Number(result.expenses)).toEqual(5000)
     expect(Number(result.investments)).toEqual(3000)
     expect(Number(result.balance)).toEqual(2000)
+  })
+
+  it('Should call Prisma with correct params', async () => {
+
+    const user = await db.user.create({data: createUserParams})
+
+    await db.transaction.createMany({
+      data: [
+        {
+          amount: 5000,
+          date: new Date(),
+          name: 'RECEITA',
+          type: 'INCOME',
+          user_id: user.id
+        },
+        {
+          amount: 5000,
+          date: new Date(),
+          name: 'RECEITA',
+          type: 'INCOME',
+          user_id: user.id
+        },
+        {
+          amount: 2000,
+          date: new Date(),
+          name: 'despesa',
+          type: 'EXPENSE',
+          user_id: user.id
+        },
+        {
+          amount: 3000,
+          date: new Date(),
+          name: 'despesa',
+          type: 'EXPENSE',
+          user_id: user.id
+        },
+        {
+          amount: 1000,
+          date: new Date(),
+          name: 'investimento',
+          type: 'INVESTMENT',
+          user_id: user.id
+        },
+        {
+          amount: 2000,
+          date: new Date(),
+          name: 'investimento',
+          type: 'INVESTMENT',
+          user_id: user.id
+        }
+      ]
+    })
+
+    const sut = new GetUserBalanceRepository()
+
+    const dbSpy = jest.spyOn(db, '$queryRaw')
+    
+    await sut.execute(user.id)
+    
+    expect(dbSpy).toHaveBeenCalled()
   })
 })
