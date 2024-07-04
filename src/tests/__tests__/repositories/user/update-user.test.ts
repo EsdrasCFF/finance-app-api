c2c423e858d772a6eede486aa5fb5cc8a6127868
Esdras Castro <ecff.dev@gmail.com>
2024-07-04 20:17:11 -0300

    test: ensure that UpdateUserRepository to update an user

---
+++ src/tests/__tests__/repositories/user/update-user.test.ts
@@ -0,0 +1,26 @@
+import { faker } from "@faker-js/faker"
+import { db } from "../../../../lib/prisma"
+import { UpdateUserRepository } from "../../../../repositories/user/update-user"
+import { createUserParams, updateUserParams } from "../../../fixtures/user"
+
+describe('UpdateUserRepository', () => {
+  
+  it('Should update user on db', async () => {
+    const user = await db.user.create({
+      data: createUserParams
+    })
+
+    const sut = new UpdateUserRepository()
+
+    const first_name = faker.person.firstName()
+    const last_name = faker.person.lastName()
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+
+    const result = await sut.execute(user.id, { first_name, last_name, email, password})
+
+    expect(result.email).toEqual(email)
+    expect(result.first_name).toEqual(first_name)
+    expect(result.last_name).toEqual(last_name)
+  })
+})
