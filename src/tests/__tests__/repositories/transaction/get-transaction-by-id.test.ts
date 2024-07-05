a2bfa79a67abb6e7d7eee59102148579025170ec
Esdras Castro <ecff.dev@gmail.com>
2024-07-04 21:11:46 -0300

    test: ensure that transaction if found by id with GetTransactionByIdRespository

---
+++ src/tests/__tests__/repositories/transaction/get-transaction-by-id.test.ts
@@ -0,0 +1,18 @@
+import { da } from "@faker-js/faker"
+import { db } from "../../../../lib/prisma"
+import { createUserParams } from "../../../fixtures/user"
+import { createTransactionParams } from "../../../fixtures/transaction"
+import { GetTransactionByIdRepository } from "../../../../repositories/transaction/get-transaction-by-id"
+
+describe('GetTransactionByIdRepository', () => {
+  it('Should get transaction by id', async () => {
+    const user = await db.user.create({data: createUserParams})
+    const transaction = await db.transaction.create({data: {...createTransactionParams, user_id: user.id}})
+
+    const sut = new GetTransactionByIdRepository()
+
+    const result = await sut.execute(transaction.id)
+
+    expect(result?.id).toEqual(user.id)
+  })
+})
