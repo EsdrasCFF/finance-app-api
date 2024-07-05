d47546c2023c5f53a1181279f9c58fccc935b1c1
Esdras Castro <ecff.dev@gmail.com>
2024-07-04 21:14:32 -0300

    test: ensure that Prisma is called correctly with GetTransactionByIdRepository

--- src/tests/__tests__/repositories/transaction/get-transaction-by-id.test.ts
+++ src/tests/__tests__/repositories/transaction/get-transaction-by-id.test.ts
@@ -15,4 +15,21 @@ describe('GetTransactionByIdRepository', () => {
 
     expect(result?.id).toEqual(transaction.id)
   })
+
+  it('Should call Prisma with correct params', async () => {
+    const user = await db.user.create({data: createUserParams})
+    const transaction = await db.transaction.create({data: {...createTransactionParams, user_id: user.id}})
+
+    const sut = new GetTransactionByIdRepository()
+
+    const dbSpy = jest.spyOn(db.transaction, 'findUnique')
+
+    await sut.execute(transaction.id)
+
+    expect(dbSpy).toHaveBeenCalledWith({
+      where: {
+        id: transaction.id
+      }
+    })
+  })
 })
