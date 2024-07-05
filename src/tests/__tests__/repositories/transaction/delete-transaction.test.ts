5b2df3b05db43aeeb99d6e959e7adc9cec228bfb
Esdras Castro <ecff.dev@gmail.com>
2024-07-04 21:06:14 -0300

    test: //TODO error

---
+++ src/tests/__tests__/repositories/transaction/delete-transaction.test.ts
@@ -0,0 +1,31 @@
+import { db } from "../../../../lib/prisma"
+import { DeleteTransactionRepository } from "../../../../repositories/transaction/delete-transaction"
+import { DeleteUserRepository } from "../../../../repositories/user/delete-user"
+import { createTransactionParams } from "../../../fixtures/transaction"
+import { createUserParams } from "../../../fixtures/user"
+
+describe('DeleteTransactionRepository', () => {
+  it('Should delete transaction on db', async () => {
+    const user = await db.user.create({data: createUserParams})
+
+    const sut = new DeleteTransactionRepository()
+
+    const result = await sut.execute(user.id)
+
+    expect(result).toBeTruthy()
+    expect(result.id).toEqual(user.id)
+  })
+
+  // it('Should call Prisma with correct params', async () => {
+  //   const user = await db.user.create({data: createUserParams})
+  //   const transaction = await db.transaction.create({data: {...createTransactionParams, user_id: user.id}})
+
+  //   const sut = new DeleteTransactionRepository()
+
+  //   const dbSpy = jest.spyOn(db.transaction, 'delete')
+
+  //   await sut.execute(transaction.id)
+
+  //   expect(dbSpy).toHaveBeenCalled()
+  // })
+})
