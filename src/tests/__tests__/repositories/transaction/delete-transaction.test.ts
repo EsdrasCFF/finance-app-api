18d8ffea67381aed9fd77bc05308f1519e9ddaee
Esdras Castro <ecff.dev@gmail.com>
2024-07-05 08:49:23 -0300

    test: ensure that transaction is deleted correcly

--- src/tests/__tests__/repositories/transaction/delete-transaction.test.ts
+++ src/tests/__tests__/repositories/transaction/delete-transaction.test.ts
@@ -7,13 +7,14 @@ import { createUserParams } from "../../../fixtures/user"
 describe('DeleteTransactionRepository', () => {
   it('Should delete transaction on db', async () => {
     const user = await db.user.create({data: createUserParams})
+    const transaction = await db.transaction.create({data: {...createTransactionParams, user_id: user.id}})
 
     const sut = new DeleteTransactionRepository()
 
-    const result = await sut.execute(user.id)
+    const result = await sut.execute(transaction.id)
 
     expect(result).toBeTruthy()
-    expect(result.id).toEqual(user.id)
+    expect(result.id).toEqual(transaction.id)
   })
 
   // it('Should call Prisma with correct params', async () => {
