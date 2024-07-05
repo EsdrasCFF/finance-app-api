f14f293fd2d6b7879f6a9523a8310c46079e814d
Esdras Castro <ecff.dev@gmail.com>
2024-07-04 21:24:08 -0300

    test: ensure that prisma is called with correct params in GetTransactionsByUserIdRepository

--- src/tests/__tests__/repositories/transaction/get-transaction-by-userId.test.ts
+++ src/tests/__tests__/repositories/transaction/get-transaction-by-userId.test.ts
@@ -1,3 +1,4 @@
+import { da } from "@faker-js/faker"
 import { db } from "../../../../lib/prisma"
 import { GetTransactionsByUserIdRepository } from "../../../../repositories/transaction/get-transactions-by-userId"
 import { transactionData, transactionsData } from "../../../fixtures/transaction"
@@ -7,7 +8,7 @@ describe('GetTransactionByUserId', () => {
   it('Should get transactions by UserId', async () => {
     const user = await db.user.create({data: createUserParams})
 
-    const transactions = await db.transaction.createMany({
+    await db.transaction.createMany({
       data: transactionsData.map((data) => {
         return {...data, user_id: user.id}
       })
@@ -19,5 +20,27 @@ describe('GetTransactionByUserId', () => {
 
     expect(result).toBeTruthy()
     expect(result.length).toBe(2)
+  })
+
+  it('Shoudl call Prisma with correct params', async () => {
+    const user = await db.user.create({data: createUserParams})
+
+    await db.transaction.createMany({
+      data: transactionsData.map((data) => {
+        return {...data, user_id: user.id}
+      })
+    })
+
+    const dbSpy = jest.spyOn(db.transaction, 'findMany')
+
+    const sut = new GetTransactionsByUserIdRepository()
+
+    await sut.execute(user.id)
+
+    expect(dbSpy).toHaveBeenCalledWith({
+      where: {
+        user_id: user.id
+      }
+    })
   })
 })
