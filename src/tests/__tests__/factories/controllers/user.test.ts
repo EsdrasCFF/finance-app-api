91f159353c232e8bcbff6bd9db861f07aaa13127
Esdras Castro <ecff.dev@gmail.com>
2024-08-05 15:51:27 -0300

    test: ensure that user controller factories are tested

--- src/tests/__tests__/factories/controllers/user.test.ts
+++ src/tests/__tests__/factories/controllers/user.test.ts
@@ -1,18 +1,28 @@
 import { CreateUserController } from "../../../../controllers/user/create-user"
 import { DeleteUserController } from "../../../../controllers/user/delete-user"
+import { GetUserBalanceController } from "../../../../controllers/user/get-user-balance"
 import { GetUserByIdController } from "../../../../controllers/user/get-user-by-id"
-import { makeCreateUserController, makeDeleteUserController, makeGetUserByIdController } from "../../../../factories/controllers/users"
+import { UpdateUserController } from "../../../../controllers/user/update-user"
+import { makeCreateUserController, makeDeleteUserController, makeGetUserBalanceController, makeGetUserByIdController, makeUpdateUserController } from "../../../../factories/controllers/users"
 
 describe('Transaction Controller Factories', () => {
   it('Should return a valid GetUserByIdController instance', () => {
-    expect(makeGetUserByIdController).toBeInstanceOf(GetUserByIdController)
+    expect(makeGetUserByIdController()).toBeInstanceOf(GetUserByIdController)
   })
 
   it('Should return a valid GetUserByIdController instance', () => {
-    expect(makeCreateUserController).toBeInstanceOf(CreateUserController)
+    expect(makeCreateUserController()).toBeInstanceOf(CreateUserController)
   })
 
   it('Should return a valid DeleteUserController instance', () => {
-    expect(makeDeleteUserController).toBeInstanceOf(DeleteUserController)
+    expect(makeDeleteUserController()).toBeInstanceOf(DeleteUserController)
+  })
+
+  it('Should return a valid DeleteUserController instance', () => {
+    expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController)
+  })
+  
+  it('Should return a valid DeleteUserController instance', () => {
+    expect(makeGetUserBalanceController()).toBeInstanceOf(GetUserBalanceController)
   })
 })
