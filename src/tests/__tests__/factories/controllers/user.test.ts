c46244c0b83b97919eff836a29daf3935951f94c
Esdras Castro <ecff.dev@gmail.com>
2024-08-05 15:47:26 -0300

    test: ensure that makeCreateUserController return correct instance

--- src/tests/__tests__/factories/controllers/user.test.ts
+++ src/tests/__tests__/factories/controllers/user.test.ts
@@ -1,8 +1,18 @@
+import { CreateUserController } from "../../../../controllers/user/create-user"
+import { DeleteUserController } from "../../../../controllers/user/delete-user"
 import { GetUserByIdController } from "../../../../controllers/user/get-user-by-id"
-import { makeGetUserByIdController } from "../../../../factories/controllers/users"
+import { makeCreateUserController, makeDeleteUserController, makeGetUserByIdController } from "../../../../factories/controllers/users"
 
 describe('Transaction Controller Factories', () => {
   it('Should return a valid GetUserByIdController instance', () => {
     expect(makeGetUserByIdController).toBeInstanceOf(GetUserByIdController)
+  })
+
+  it('Should return a valid GetUserByIdController instance', () => {
+    expect(makeCreateUserController).toBeInstanceOf(CreateUserController)
+  })
+
+  it('Should return a valid DeleteUserController instance', () => {
+    expect(makeDeleteUserController).toBeInstanceOf(DeleteUserController)
   })
 })
