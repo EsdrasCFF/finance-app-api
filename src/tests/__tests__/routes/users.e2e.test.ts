3fd7c12fa17b0cb120ec0c02a34eb554f571cf93
Esdras Castro <ecff.dev@gmail.com>
2024-08-05 23:24:19 -0300

    test: ensure that delete user route is run correctly

--- src/tests/__tests__/routes/users.e2e.test.ts
+++ src/tests/__tests__/routes/users.e2e.test.ts
@@ -1,6 +1,5 @@
 import fastify from 'fastify'
 import supertest from 'supertest'
-import { createUser } from '../../../routes/users/create-user'
 import { createUserParams } from '../../fixtures/user'
 import { app } from '../../../server'
 
@@ -17,9 +16,24 @@ describe('User Routes E2E Tests', () => {
   it('POST /users should return 201 when user is created', async () => {
     const response = await supertest(app.server)
       .post('/api/users')
-      .set('Content-Type', 'application/json')
       .send(createUserParams)
       
     expect(response.status).toBe(201)
+  })
+
+  it('POST /users should return 201 when user is created', async () => {
+    const createUser = await supertest(app.server)
+      .post('/api/users')
+      .send({
+        ...createUserParams,
+        email: 'novoemail@email.com'
+      })
+      
+    const userId = createUser.body.data.id
+    const response = await supertest(app.server)
+      .delete(`/api/users/${userId}`)
+      
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('data')
   })
 })
