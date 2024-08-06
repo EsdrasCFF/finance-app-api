b5648d305a3a86e87149716ea4f80ae1c7c7a991
Esdras Castro <ecff.dev@gmail.com>
2024-08-06 11:23:21 -0300

    test: add tests E2E   with supertest to routes

--- src/tests/__tests__/routes/users.e2e.test.ts
+++ src/tests/__tests__/routes/users.e2e.test.ts
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
 import supertest from 'supertest'
-import { createUserParams } from '../../fixtures/user'
+import { createUserParams, updateUserParams } from '../../fixtures/user'
 import { app } from '../../../server'
 import { createTransactionParams } from '../../fixtures/transaction'
 
@@ -22,7 +22,7 @@ describe('User Routes E2E Tests', () => {
     expect(response.status).toBe(201)
   })
 
-  it('POST /users should return 201 when user is created', async () => {
+  it('DELETE /users should return 201 when user is deleted', async () => {
     const createUser = await supertest(app.server)
       .post('/api/users')
       .send({
@@ -38,7 +38,7 @@ describe('User Routes E2E Tests', () => {
     expect(response.body).toHaveProperty('data')
   })
 
-  it(`POST /users/userId/balance`, async () => {
+  it(`GET /users/userId/balance returns 200 when get is successful`, async () => {
     const createUser = await supertest(app.server)
       .post('/api/users')
       .send({
@@ -59,5 +59,36 @@ describe('User Routes E2E Tests', () => {
     .get(`/api/users/${userId}/balance`)
     
     expect(response.status).toEqual(200)
+  })
+
+  it('GET /users/userId returns 200 when get is successfully ', async () => {
+    const createUserRoute = await supertest(app.server)
+      .post('/api/users')
+      .send(createUserParams)
+    
+
+    const userId = createUserRoute.body.data.id
+    const response = await supertest(app.server)
+      .get(`/api/users/${userId}`)
+    
+    expect(response.status).toBe(200)
+  })
+
+  it('PATCH /users/userId return 200 when user is updated' , async () => {
+    const createUserRoute = await supertest(app.server)
+      .post('/api/users')
+      .send(createUserParams)
+    
+    const userid = createUserRoute.body.data.id
+
+    const response = await supertest(app.server)
+      .patch(`/api/users/${userid}`)
+      .send({
+        ...updateUserParams,
+        first_name: 'New Name'
+      })
+    
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('data')
   })
 })
